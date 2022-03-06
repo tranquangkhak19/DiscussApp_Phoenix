fa75933760a685a31df3b3e130c35f613eb55492
unknown <tqk19112001@gmail.com>
2022-03-06 15:14:49 +0700

    Handling failed form submissions and styling with CSS in Phoenix

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -1,8 +1,9 @@
 defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
   alias Discuss.Topic
+  alias Discuss.Repo
 
-  def new(conn, params) do
+  def new(conn, _params) do
     # struct = %Discuss.Topic{}
     # params = %{}
     # changeset = Discuss.Topic.changeset(struct, params)
@@ -14,5 +15,12 @@ defmodule DiscussWeb.TopicController do
   def create(conn, %{"topic" => topic}) do
     #IO.inspect(params)
     #%{"topic" => topic} = params
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, post} -> IO.inspect(post)
+      {:error, changeset} ->
+        render conn, "new.html", changeset: changeset
+    end
   end
 end
