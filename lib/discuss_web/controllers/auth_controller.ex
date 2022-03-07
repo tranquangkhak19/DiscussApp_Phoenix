6e63c56f51ce95fdb2febf77b7040d0aa078639f
unknown <tqk19112001@gmail.com>
2022-03-07 17:32:14 +0700

    The user changset

--- lib/discuss_web/controllers/auth_controller.ex
+++ lib/discuss_web/controllers/auth_controller.ex
@@ -2,8 +2,21 @@ defmodule Discuss.AuthController do
   use DiscussWeb, :controller
   plug Ueberauth
 
-  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
-    IO.inspect(auth)
+  alias Discuss.User
 
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
+    user_params = %{token: auth.credentials.token, email: auth.info.email, provider:"github"}
+    changeset = User.changeset(%User{}, user_params)
+
+    insert_or_update(changeset)
+  end
+
+  defp insert_or_update_user(changeset) do
+    case Repo.get_by(User, email: changset.changes.email) do
+      nil ->
+        Repo.insert(changeset) #Inserted with succes -> return {:ok, struct}
+      user ->
+        {:ok, user} #same type with the above case
+    end
   end
 end
